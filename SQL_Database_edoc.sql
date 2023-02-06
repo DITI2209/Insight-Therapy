effe84d88feb3d1f0802f75a60dd0091f8baee51
Diti Divekar <79137306+DITI2209@users.noreply.github.com>
2023-02-06 22:10:45 +0530

    Update SQL_Database_edoc.sql

--- SQL_Database_edoc.sql
+++ SQL_Database_edoc.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: Jun 19, 2022 at 01:39 PM
--- Server version: 5.7.26
--- PHP Version: 7.3.5
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
